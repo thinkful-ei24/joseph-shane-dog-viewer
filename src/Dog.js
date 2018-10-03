657211b42eb8c4fcfb290098eea8703fc8c1fd06
unknown <slupton89@gmail.com>
2018-10-03 10:42:56 -0600

    finished chooser, started output

--- src/Dog.js
+++ src/Dog.js
@@ -2,12 +2,22 @@ import React, { Component } from "react";
 
 import "./Dog.css";
 import DogChooser from "./dogChooser";
+import DogOut from "./dogOut";
 
 export default class Dog extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      currentDog: 'affenpinscher'
+    }
   }
   render() {
-    return <DogChooser dogs={this.props.dogs} />;
+    return (
+      <div>
+      <DogChooser dogs={this.props.dogs} currentDog={currentDog => this.setState({currentDog})}  />
+      <DogOut currentDog={this.state.currentDog} />
+      </div>
+    )
   }
 }
